66600cb3980ad800b1aab194a8f4d21d55290c76
John Doe <help@aww.life>
2025-12-20 19:30:42 +0300

    support lid

--- src/types/WhatsAppServer.ts
+++ src/types/WhatsAppServer.ts
@@ -3,4 +3,8 @@ import { Whatsapp } from '@wppconnect-team/wppconnect';
 export interface WhatsAppServer extends Whatsapp {
   urlcode: string;
   status: string;
+  lidEntryCache?: Record<
+    string,
+    import('@wppconnect/wa-js/dist/contact').PnLidEntryResult
+  >;
 }
